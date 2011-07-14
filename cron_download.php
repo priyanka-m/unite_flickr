05f67b0e702bb8033758d7626eba2ced91726d3c
Priyanka M <priyanka.menghani@gmail.com>
2011-07-14 10:14:12 +0530

    cron_download function defined

---
+++ cron_download.php
@@ -0,0 +1,13 @@
+<?php
+  
+  function flickr_cron_download() {
+    /* 
+     * This function should contain the logic for reading the unite
+     * table and downloading the files which are eligible for 
+     * downloading, in the respective owncloud users directory
+     *
+     */
+     //TODO Complete this function
+     echo 'Dummy';
+  }
+?>
